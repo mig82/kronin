2b8388b7645aca5a5a1cf7d28125c78c8c824df8
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-12-13 14:02:20 +0100

    Wire Q without the need to customize it

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-/*!
-*
-* Krōnin is a collection of convenient extensions to enhance the Kony SDK.
-* @author Miguelángel Fernández.
-*/
+
+require("./aaa");
 
 require("./router");
 require("./mvc.wire");
@@ -21,19 +18,24 @@ require("./i18n.getCurrencyAmount");
 require("./i18n.getFormattedAmount");
 require("./i18n.getThousandsSeparator");
 
+require("./animations");
 require("./animations.rotate");
 
 //In order to use Q, we'll need to first define a global setTimeout function.
 require("./setTimeout");
-//Make it possible for Q.js to attach itself to the kony global var as kony.Q.
-if (typeof self === "undefined"){
-	self = kony;
+// The npm postinstall script will have placed a minified version of Q in the
+// src folder. Since Q's set-up will consider this a CommonJS environment, we
+// can require it and then declare it as a global.
+const Q = require("./q");
+if(typeof Q === "undefined"){
+	alert("Q was not properly defined!");
 }
-require("q");
-var Q = kony.Q;
-Q.longStackSupport = true;
-require("./q2k.ui.animate");
-require("./q2k.theme.setCurrentTheme");
-require("./q2k.fabric.init");
-require("./q2k.fabric.login");
-require("./q2k.fabric.invoke");
+else{
+	kony.print("kronin says: Q is set up and ready to go!");
+	Q.longStackSupport = true;
+	require("./q2k.ui.animate");
+	require("./q2k.theme.setCurrentTheme");
+	require("./q2k.fabric.init");
+	require("./q2k.fabric.login");
+	require("./q2k.fabric.invoke");
+}
