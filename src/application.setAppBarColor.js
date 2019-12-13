1e6944a8ae11f08187164158518baeac5dd9c18d
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-12-13 01:30:04 +0100

    Make setAppBarColor safer to use
    Prepend # if missing, wrap in a try/catch statement, log any error, etc.

--- src/application.setAppBarColor.js
+++ src/application.setAppBarColor.js
@@ -1,14 +1,26 @@
-/**
- * setAppBarColor - Sets the background color of the Android status bar.
- *
- * @param  {String} color An RGB hex-based color code —e.g. #cc0000 for a dark red.
- * @author Miguelángel Fernández
- */
-(() => {
-	function setAppBarColor(color) {
-		kony.application.setApplicationProperties({
-			statusBarColor: color
-		});
+((definition) => {
+	/**
+	* setAppBarColor - Sets the background color of the Android status bar.
+	*
+	* @param  {String} color An RGB hex-based color code —e.g. #cc0000 for a dark red.
+	*/
+	kony.application.setAppBarColor = definition;
+})((color) => {
+
+	if(typeof color === "undefined" || color.length === 0){
+		return;
 	}
-	kony.application.setAppBarColor = setAppBarColor;
-})();
+	if(color[0] !== "#"){
+		color = "#" + color;
+	}
+	if(typeof kony.application.setApplicationProperties === "function"){
+		try{
+			kony.application.setApplicationProperties({
+				statusBarColor: color
+			});
+		}
+		catch(e){
+			kony.print(`Error trying to set the app's bar color to ${color}.`);
+		}
+	}
+});
