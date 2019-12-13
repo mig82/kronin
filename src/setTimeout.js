be817abf9f1ebe2fb4ed96b0c04092ad727048a3
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-12-13 14:04:55 +0100

    Add EsLint config exceptions to setTimeout polyfill

--- src/setTimeout.js
+++ src/setTimeout.js
@@ -18,30 +18,28 @@
  * to window or self- in non-browser environments, so there would be nothing to attach
  * it to. Therefore it just has to be declared as a global variable.
  *
- * 3) Prefix "000" to module name is important to try and force this to be loaded before
+ * 3) When used on its own, give this module prefix "aaa" to force this to be loaded before
  * any other javascript libraries that might be dependant setTimeout and clearTimeout
  * being already defined.
- *
- * Author: Miguelangel Fernandez, miguelangel.fernandez@kony.com
- *
  */
-var _setTimeout = (function(){
+
+var _setTimeout = (function(){ // eslint-disable-line no-unused-vars
 	var timeoutId = 7000;
 	return function (fn, msDelay){
 		kony.timer.schedule(""+timeoutId, fn, msDelay/1000, false);
 		return timeoutId++;
-	}
+	};
 })();
 
-var _clearTimeout = function(timeoutId){
+var _clearTimeout = function(timeoutId){ // eslint-disable-line no-unused-vars
 	kony.timer.cancel(timeoutId);
-}
+};
 
 if(typeof window === "undefined" && typeof self === "undefined"){
-	eval("var setTimeout = _setTimeout");
-	eval("var clearTimeout = _clearTimeout");
-	kony.print("Had to monkeypatch setTimeout to: " + setTimeout);
+	eval("var setTimeout = _setTimeout"); // eslint-disable-line no-eval
+	eval("var clearTimeout = _clearTimeout"); // eslint-disable-line no-eval
+	kony.print("Kronin says: defined setTimeout to: " + setTimeout);
 }
 else{
-	kony.print("setTimeout is natively supported as: " + setTimeout);
+	kony.print("Kronin says: setTimeout is natively supported as: " + setTimeout);
 }
