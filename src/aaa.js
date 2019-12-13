4be843561f6854a44dd37960c4cda2297180d5c9
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-12-13 14:02:50 +0100

    Add a header with the description of Kronin

---
+++ src/aaa.js
@@ -0,0 +1,9 @@
+/*!
+*
+* Krōnin is a collection of convenient extensions to enhance the Kony SDK.
+* https://www.npmjs.com/package/kronin
+* @author Miguelángel Fernández.
+*/
+
+//This avoids plaguing the editor with EsLint errors for the require, module, exports used by Browserify.
+/*eslint no-unused-vars: "warn"*/
