070194bc26d57531775a5536d31645f5a4e98312
Miguelángel Fernández <miguelangelxfm@gmail.com>
2020-01-30 15:52:10 +0100

    Sort modules to be imported

--- src/main.js
+++ src/main.js
@@ -1,27 +1,32 @@
 
 require("./aaa");
 
-require("./router");
-require("./mvc.wire");
-
-require("./ui.getDescendants");
-require("./ui.getComponents");
-
-require("./getOs");
 require("./amplify");
-require("./application.setAppBarColor");
-require("./i18n.getLocalizedString2");
-require("./mvc.genAccessors");
-
-require("./i18n.getCurrentLocale2");
-require("./i18n.getCurrencyAmount");
-require("./i18n.getFormattedAmount");
-require("./i18n.getThousandsSeparator");
 
 require("./animations");
-require("./animations.rotate");
 require("./animations.animate");
 require("./animations.reveal");
+require("./animations.rotate");
+
+require("./application.setAppBarColor");
+
+require("./getOs");
+
+require("./i18n.getCurrencyAmount");
+require("./i18n.getCurrentLocale2");
+require("./i18n.getDecimalSeparator");
+require("./i18n.getFormattedAmount");
+require("./i18n.getLocalizedString2");
+require("./i18n.getThousandsSeparator");
+require("./i18n.localizeWidget");
+
+require("./mvc.genAccessors");
+require("./mvc.wire");
+require("./router");
 
 require("./timer.cancel2");
 require("./timer.schedule2");
+
+require("./ui.getComponents");
+require("./ui.getDescendants");
+//require("./ui.getScreenSize");
