ac5aa6617ac8d19251f8c21ad5337d341770dcba
Miguelángel Fernández <miguelangelxfm@gmail.com>
2020-01-30 12:27:28 +0100

    Remove need idPrefix parameter from schedule2

--- src/timer.schedule2.js
+++ src/timer.schedule2.js
@@ -2,21 +2,20 @@ require("./timer.cancel2");
 
 ((definition) => {
 	/**
-	 * kony.timer.schedule2 - Safely schedule a timer. It behaves just like kony.timer.schedule but
-	 * instead of expecting a unique timerId, it just expects a prefix —which is
-	 * not required to be unique— and then appends a randomised suffix to it, thus
-	 * guaranteeing that the timer will always be created.
+	 * kony.timer.schedule2 - Safely schedule a timer. It behaves just like
+	 * kony.timer.schedule but, instead of expecting a unique timerId, it just
+	 * generates one for you and returns it, thus avoiding the need to specify
+	 * a unique ID every time.
 	 *
-	 * @param  {string} idPrefix The prefix that will be used to compose a unique identifier for the timer.
 	 * @param  {function} fn The function to be invoked.
 	 * @param  {number} delay the time, in seconds, the timer should wait before fn is executed.
 	 * @param  {boolean} repeat Whether or not to execute fn more than once.
 	 * @return {string} The unique identifier of the newly created timer.
 	 */
 	kony.timer.schedule2 = definition;
-})((idPrefix, fn, delay, repeat) => {  //eslint-disable-line no-unused-vars
-	
-	var timerId = idPrefix + "_" + Math.random();
+})((fn, delay, repeat) => {  //eslint-disable-line no-unused-vars
+
+	var timerId = "kronin." + repeat?"intr_":"tout_" + Math.random();
 	kony.timer.schedule(timerId, () => {
 		fn();
 		if(!repeat) kony.timer.cancel2(timerId);
