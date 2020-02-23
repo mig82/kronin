4bc5b6064f3d39ddf544d70ea1511f0d869ef8d0
Miguelángel Fernández <miguelangelxfm@gmail.com>
2020-02-23 11:28:11 +0100

    Fix def of i18n.localizeWidget

--- src/i18n.localizeWidget.js
+++ src/i18n.localizeWidget.js
@@ -5,11 +5,9 @@ require("./i18n.getLocalizedString2");
 	 * Shorthand function to localize the text on a widget.
 	 * */
 	kony.i18n.localizeWidget = definition;
-})(() => {
-	(widget) => {
-		var localize = kony.i18n.getLocalizedString2 || kony.i18n.getLocalizedString;
-		if(widget.text){
-			widget.text = localize(widget.text) || widget.text;
-		}
+})((widget) => {
+	var localize = kony.i18n.getLocalizedString2 || kony.i18n.getLocalizedString;
+	if(widget.text){
+		widget.text = localize(widget.text) || widget.text;
 	}
 });
